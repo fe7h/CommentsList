6ad9ee02d3e2020b4f91829b7acbee4ba7be093d
fesh <189043093+fe7h@users.noreply.github.com>
2025-05-23 00:20:18 +0300

    added raw implementation for comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+# from django.contrib.contenttypes.models import
 
-# Create your models here.
+
+class Comment(models.Model):
+    time_create = models.DateTimeField(auto_now_add=True)
+    # ip_address = models.IPAddressField()
+    # тут скорее нужна связаная модель с наборм комнентов и уникальным хешем на их основе
+
+    parent_comment = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='nested_comments',
+        blank=True
+    )
+
+    user_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    home_page = models.URLField(blank=True)
+    text = models.TextField(max_length=5000)
+
+    class Meta:
+        pass
