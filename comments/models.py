3a32cc30d4dfdb0c003e830475ea1801bbd454d8
fesh <189043093+fe7h@users.noreply.github.com>
2025-05-23 02:20:02 +0300

    split Comment model into subclasses, renamed Comment to BaseComment and inherit from PolymorphicModel

--- comments/models.py
+++ comments/models.py
@@ -1,23 +1,44 @@
 from django.db import models
-# from django.contrib.contenttypes.models import
+from polymorphic.models import PolymorphicModel
 
 
-class Comment(models.Model):
+# class UserData(models.Model):
+#     pass
+# модель с наборм данных с запроса пользователя(юзерагент апи и тд) и уникальным хешем на их основе
+
+# attached_media
+# class AttachedFile(models.Model):
+#     pass
+#
+#
+# class AttachedImage(models.Model):
+#     pass
+
+
+class BaseComment(PolymorphicModel):
     time_create = models.DateTimeField(auto_now_add=True)
-    # ip_address = models.IPAddressField()
-    # тут скорее нужна связаная модель с наборм комнентов и уникальным хешем на их основе
-
-    parent_comment = models.ForeignKey(
-        'self',
-        on_delete=models.CASCADE,
-        related_name='nested_comments',
-        blank=True
-    )
+    # user_data = models.ForeignKey(
+    #         UserData,
+    #         on_delete=models.PROTECT,
+    #         related_name='comments',
+    #     )
 
     user_name = models.CharField(max_length=30)
     email = models.EmailField()
     home_page = models.URLField(blank=True)
     text = models.TextField(max_length=5000)
 
-    class Meta:
-        pass
+    # nested_comments = GenericRelation('NestedComment')
+
+
+class TopComment(BaseComment):
+    pass
+
+
+class NestedComment(BaseComment):
+    parent_comment = models.ForeignKey(
+        BaseComment,
+        on_delete=models.CASCADE,
+        related_name='nested_comments',
+        blank=True
+    )
