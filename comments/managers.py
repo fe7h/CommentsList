4b357a8f72fe5ad2863d3e3add4133a5cc854f22
fesh <189043093+fe7h@users.noreply.github.com>
2025-05-26 02:14:11 +0300

    fix recursive in CommentRecursiveSelectionManager

--- comments/managers.py
+++ comments/managers.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from polymorphic.managers import PolymorphicManager
+from polymorphic.managers import PolymorphicManager, PolymorphicQuerySet
 
 from django_cte import With, CTEManager
 
@@ -10,40 +10,70 @@ def laze_load():
     return NestedComment
 
 
+def laze_load2():
+    from .models import BaseComment
+    return BaseComment
+
+# qs = TopComment.tree(root_pk=5).all()
+# PolymorphicQuerySet(model=BaseComment, query=qs.query, using=qs._db)
+
+
+# RecursiveChildManager
 class CommentRecursiveSelectionManager(models.Manager):
+    """
+        nested_comments
+        'parent_comment'_id
+        laze_load() -> NestedComment дочерняя модель что может ссылаться сам на себя
+        self.model -> TopComment родительская модель (высшая в звене)
 
-    make_cte = None
+    """
 
-    def __call__(self, pk):
-        self.make_cte = self._make_cte(pk)
-        return self
+    # make_cte = None
 
-    def get_queryset(self):
-        if not self.make_cte:
+    def __call__(self, root_pk):
+        # self.make_cte = self._make_cte(pk)
+        return self.get_queryset(self._make_cte(root_pk))
+
+    def get_queryset(self, make_cte=None):
+        if not make_cte:
             raise Exception
-        queryset = self.name_placeholder()
-        del self.make_cte
-        return queryset
 
-    def name_placeholder(self):
-        cte = With.recursive(self.make_cte)
+        cte = With.recursive(make_cte)
 
         queryset = cte.join(
-            laze_load().cte_objects.all(),
-            parent_comment_id=cte.col.nested_comments
+            laze_load2().cte_objects.all(),
+            id=cte.col.id
         ).with_cte(cte)
 
+        queryset = PolymorphicQuerySet(
+            model=laze_load2(),
+            query=queryset.query,
+            using=queryset._db
+        )
+        # queryset = cte.join(laze_load().cte_objects.all()).with_cte(cte)
         return queryset
 
-    def _make_cte(self, pk):
+    # @staticmethod
+    # def name_placeholder(make_cte):
+    #     cte = With.recursive(make_cte)
+    #
+    #     queryset = cte.join(
+    #         laze_load().cte_objects.all(),
+    #         parent_comment_id=cte.col.nested_comments
+    #     ).with_cte(cte)
+    #
+    #     return queryset
+
+    def _make_cte(self, root_pk):
         def inner(cte):
-            return self.model.cte_objects.filter(pk=pk).values('nested_comments').union(
+            return (self.model.cte_objects.filter(pk=root_pk).values('id')
+            .union(
                 cte.join(
                     laze_load(),
-                    parent_comment_id=cte.col.nested_comments
-                ).values('nested_comments'),
+                    parent_comment=cte.col.id
+                ).values('id'),
                 all=True
-            )
+            ))
         return inner
 
 
