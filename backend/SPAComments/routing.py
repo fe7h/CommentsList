a906257a9ef93023ecf32a66f90139cc6b491478
fesh <189043093+fe7h@users.noreply.github.com>
2025-06-18 04:08:13 +0300

    changed file structure

--- backend/SPAComments/routing.py
+++ backend/SPAComments/routing.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from channels.routing import URLRouter
 
-from comments.routing import websocket_urlpatterns as comment_urls
+from comments.websocket.routing import websocket_urlpatterns as comment_urls
 
 websocket_urlpatterns = [
     path('ws/', URLRouter([
