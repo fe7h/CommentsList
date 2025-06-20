8816b45ad0308149d8bdd82b5abaf0b9f5b94379
fesh <189043093+fe7h@users.noreply.github.com>
2025-06-20 07:40:19 +0300

    fix asgi.py

--- backend/SPAComments/asgi.py
+++ backend/SPAComments/asgi.py
@@ -1,18 +1,20 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from .routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SPAComments.settings')
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from .routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
         )
     ),
 })
-
